73140500e881bc05f3ae2ed5b55d398ef2bef5a0
natalianunes111 <167426957+natalianunes111@users.noreply.github.com>
2024-08-05 08:33:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: " "
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: " "
             }
         ]
     },
@@ -22,9 +20,11 @@ const perguntas = [
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "  "
-              
+                texto: " ",
             },
+            {
+                texto: " ",
+            }
             
         ]
     },
@@ -32,20 +32,23 @@ const perguntas = [
         enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: " "              
+                texto: " ",
             },
+            {
+                texto: " ",
+            }
             
         ]
     },
     {
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
+           {
+                texto: " ",
+            },
             {
                 texto: " ",
-                        }                    ]
-    },
-    {
-       
+            }                ]
     },
 ];
 
